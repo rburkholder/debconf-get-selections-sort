d713712d57dd6eaaac98df79a7793cdc0e8d59c1
Raymond P. Burkholder <github@oneunified.net>
2015-08-11 14:54:59 -0300

    removes duplicate entries further in original seed

--- seed.sort.py
+++ seed.sort.py
@@ -12,10 +12,32 @@
 
 import sys
 import re
+import os
 
 # some output modifiers, which could be done from command line at some point
 di_only = True  # True: exclude other entries d-i entry available, False to print all entries
 answers_only = False # True: only print entries with answers
+
+def buildfields( s ):
+  fields = ()
+  match = re.match( '^(\S+)\s+(\S+)\s+(\S+)\s+(\S+)', s )
+  if ( None != match ):
+    fields = ( match.group(1), match.group(2), match.group(3), match.group(4) )
+  else:
+    match = re.match( '^(\S+)\s+(\S+)\s+(\S+)', s )
+    if ( None == match ):
+      print 'broken 1'
+      os._exit(0)
+    else:
+      fields = ( match.group(1), match.group(2), match.group(3), '' )
+  if ( 4 != len(fields) ):
+    print 'broken 2'
+    os._exit(0)
+  return fields
+
+def buildkey( fields ):
+  key = fields[0] + ' ' + fields[1] + ' ' + fields[2]
+  return key
 
 # http://stackoverflow.com/a/17927520/2730971
 f = sys.stdin
@@ -33,6 +55,7 @@ state = 0 # state transitions
 buf = ""  # accumulation of statements
 dict = {} # check for duplicate keys at some point, may therefore need a func
 key = ""
+fields = ()
 
 # parse the file and build up dictionary with entries and associated comments
 for line in f:
@@ -41,19 +64,26 @@ for line in f:
     if ( 0 < len(text) ):
       if ( '#' == text[0] ):
         if ( 1 != state ): # save what came before
-          if ( 0 != len(key) ): # if there is something to save
-            dict[key] = buf
+          if ( 0 < len(key) ):
+            if ( not ( key in dict ) ): # ignore duplicaetes further in file
+              #print 'added dict 1'
+              dict[key] = ( fields, buf )
           key = ""
           buf = ""
+          fields = ()
         buf += line
         state = 1 # state: comment
       else:
         if ( ( 'a' <= text[0] and 'z' >= text[0] ) or ( 'A' <= text[0] and 'Z' >= text[0] ) ):
           if ( 2 == state ):
-            dict[key] = buf
+            if ( not ( key in dict ) ): # ingnore  duiplicates further in file
+              #print 'added dict 2'
+              dict[key] = ( fields, buf )
             key = ""
             buf = ""
-          key = text
+            fields = ()
+          fields = buildfields( text )
+          key = buildkey( fields )
           buf += line
           state = 2  # state: regular character
         else:
@@ -65,7 +95,9 @@ for line in f:
     #print( "state %s" % (state) )
     #print line[0]
 if ( 0 < len(key) ): # finish any stragglers
-  dict[key] = buf
+  if ( not ( key in dict ) ): # ignore duplicaetes further in file
+    #print 'added dict 3'
+    dict[key] = ( fields, buf )
 
 # parse out second element of line so can sort the dictionary
 #  and build a new dictionary with multiple answers of same key
@@ -73,20 +105,15 @@ newdict = {}
 #print len(keys)
 for akey in list(dict.keys()):
   #print dict[akey]
-  if ( answers_only ):
-    match = re.match( '^(\S+)\s+(\S+)\s+(\S+)\s+(\S+)', akey )
+  #print match.group(2)
+  # maybe choice with answers here but some answers are legitimate blanks
+  newkey = dict[akey][0][1] # pull out list, pull out fields, second field in list
+  if ( newkey in newdict ):
+    #alist = newdict[newkey]
+    #alist += [ dict[akey] ]
+    newdict[newkey] += [ dict[akey] ]
   else:
-    #match = re.match( '^(\S+)\s+(\S+)\s+(\S+)', akey )
-    match = re.match( '^(\S+)\s+(\S+)', akey )
-  if ( None != match ):
-    #print match.group(2)
-    newkey = match.group(2)
-    if ( newkey in newdict ):
-      alist = newdict[newkey]
-      alist += [ ( akey, dict[akey] ) ]
-      newdict[newkey] = alist
-    else:
-      newdict[newkey] = [ ( akey, dict[akey] ) ]
+    newdict[newkey] =  [ dict[akey] ]
 
 # print out the sorted list of questions, combining debconf and d-i
 keys = list( newdict.keys() )
@@ -98,11 +125,11 @@ for akey in keys:
   if ( di_only ):
     difound = False
     for item in alist:
-      match = re.match( '^(\S+)', item[ 0 ] )
-      if ( None != match ):
-        if ( 'd-i' == match.group(1) ):
-          difound = True
-          print item[1]
+      #match = re.match( '^(\S+)', item[ 1 ][0] )
+      #if ( None != match ):
+      if ( 'd-i' == item[0][0] ):
+        difound = True
+        print item[1]
     if ( not difound ):
       for item in alist:
         print item[1]
